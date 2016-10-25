331b2975ec0aad3a4912f67cbcc121ea1c08aa43
bill bittner <bittner.w@gmail.com>
2016-10-24 21:15:10 -0700

    got most of the main askQuestion function working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,94 @@
 var questionBank = [
     {
         question: "This is question one. Now, what is the answer?",
-        answer: "answer 1",
-        wrongAnswers: ["answer 2", "answer 3", "answer 4"]
+        answers: [
+            {
+                text: "this is wrong answer 1",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 2",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 3",
+                isCorrect: false,
+            },
+            {
+                text: "this is the correct answer",
+                isCorrect: true,
+            }
+        ]            
     },
     {
-        question: "This is question two.  Please tell me: what is the answer?",
-        answer: "answer 1",
-        wrongAnswers: ["answer 2", "answer 3", "answer 4"]
+        question: "This is question two. Answer me!",
+        answers: [
+            {
+                text: "this is wrong answer 1",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 2",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 3",
+                isCorrect: false,
+            },
+            {
+                text: "this is the correct answer",
+                isCorrect: true,
+            }
+        ]            
     },
     {
-        question: "This is question three.  What is the answer?",
-        answer: "answer 1",
-        wrongAnswers: ["answer 2", "answer 3", "answer 4"]
+        question: "This is question three. What is the answer?",
+        answers: [
+            {
+                text: "this is wrong answer 1",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 2",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 3",
+                isCorrect: false,
+            },
+            {
+                text: "this is the correct answer",
+                isCorrect: true,
+            }
+        ]            
     },
     {
-        question: "This is question four. What is the answer?",
-        answer: "answer 1",
-        wrongAnswers: ["answer 2", "answer 3", "answer 4"]
-    },
+        question: "This is question four. Please, answer it?",
+        answers: [
+            {
+                text: "this is wrong answer 1",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 2",
+                isCorrect: false,
+            },
+            {
+                text: "this is wrong answer 3",
+                isCorrect: false,
+            },
+            {
+                text: "this is the correct answer",
+                isCorrect: true,
+            }
+        ]            
+    }
 ];
 var newQuestions = [];
 var usedQuestions = [];
 var timeLeft = 0;
-var questionInterval;
+var questionCountDownInterval;
+var currentQuestion;
 
 function startGame() {
     //fill an array with potential questions
@@ -44,44 +109,77 @@ function startGame() {
 
 function askQuestion() {
     //create all the page elements
+
     //create timer text
-    var timerText = $("<h2>Time Remaining: </h2>");
-    // timerText.html("Time Remaining: ");
+    var timerText = $("<h2></h2>");
     timerText.addClass("timer-elements");
+    timerText.attr("id", "time-left");
     timerText.appendTo($("#timer-area"));
     console.log("timer text was created");
-    //create time element to update
-    var timeLeftDisplay = $("<h2></h2>");
-    timeLeftDisplay.addClass("timer-elements");
-    timeLeftDisplay.attr("id", "timer-time");
-    timeLeftDisplay.appendTo(timerText);
-    console.log("time remaining was created");
-    //create question
+    
+    //choose a question
+    //pick a question in the new questions list.  note: list shoudl not be empty
+    var i = Math.floor(Math.random()*newQuestions.length);
+    //move the question to a new variable
+    currentQuestion = newQuestions[i];
+    //remove the question from the array
+    newQuestions.splice(i, 1);
 
+    //create the question text
+    var questionText = $("<h2></h2");
+    questionText.html(currentQuestion.question);
+    questionText.appendTo($("#content-area"));
 
-    //create timing aspects
+    //create an answer target div
+    var answerTarget = $("<div>");
+    answerTarget.insertAfter(questionText);
+    
+    //create answers
+    var numberOfAnswers = currentQuestion.answers.length;
+    for (i = 0; i < numberOfAnswers; i++){
+        //create the answer element
+        var answer = $("<p></p>");
+        //choose a random answer from the answers
+        var j = Math.floor(Math.random()*currentQuestion.answers.length);
+        //assign it text
+        answer.html(currentQuestion.answers[j].text);
+        //assign classes
+        if (currentQuestion.answers[j].isCorrect === true) {
+            answer.addClass("answer correct");
+        } else {
+            answer.addClass("answer incorrect");
+        };
+        //remove the answer you just chose from available answers
+        currentQuestion.answers.splice(j, 1);
+        //insert it into the html
+        answer.insertAfter(answerTarget);
+    };
+    
     //set interval to count down
     timeLeft = 30;
-    questionInterval = setInterval(countDown, 1000);
-    $("#timer-time").html(timeLeft);
-
-    //add onclicks etc.
+    questionCountDownInterval = setInterval(countDown, 1000);
+    $("#time-left").html("Time Remaining: " + timeLeft);
 };
-
-function timeOut(){
-
-}
 
 function countDown(){
     //count down one second
-    timeLeft -= 1;
+    timeLeft--;
     //display thc new time
-    $("#timer-time").html(timeLeft);
+    $("#time-left").html("Time Remaining: " + timeLeft);
     //check to see if time is update
-    if(timeLeft === 0){
+    if (timeLeft === 0){
+        clearInterval(questionCountDownInterval);
         timesUp();
     };
 };
+
+function correctAnswer(){
+
+}
+
+function wrongAnswer(){
+
+}
 
 function timesUp(){
     //clear contents
