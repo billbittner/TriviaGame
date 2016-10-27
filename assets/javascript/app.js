972a9c44677174b0b0113b2c28db9d1848bd2fa3
bill bittner <bittner.w@gmail.com>
2016-10-26 18:43:06 -0700

    renamed some classes/ids and started working on style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -125,14 +125,14 @@ function setupGame(){
         for (var i = 0; i < questionBank.length; i++){
             newQuestions.push(questionBank[i]);
         };
-        //hide the results area
-        $(".results-area").hide();
+        //hide the results content
+        $(".results-content").hide();
         //ask first question
         askQuestion();
     });
     //create on-click events for the answers
     $(".answer").on("click", checkAnswer);
-};
+}
 
 function startGame() {
     //fill an array with potential questions
@@ -140,20 +140,20 @@ function startGame() {
         newQuestions.push(questionBank[i]);
     };
     //hide everything except the start
-    $(".timer-area").hide();
-    $(".question-area").hide();
-    $(".answer-area").hide();
-    $(".results-area").hide();
-};
+    $(".timer-content").hide();
+    $(".question-content").hide();
+    $(".solution-content").hide();
+    $(".results-content").hide();
+}
 
 function askQuestion() {
-    //show and hide the proper areas
-    $(".timer-area").show();
-    $(".start-area").hide();
-    console.log("start area hidden");
-    $(".question-area").show();
-    //hid answer-area in case this is question 2, 3, 4 etc.
-    $(".answer-area").hide();
+    //show and hide the proper content
+    $(".timer-content").show();
+    $(".start-content").hide();
+    console.log("start content hidden");
+    $(".question-content").show();
+    //hid solution-content in case this is question 2, 3, 4 etc.
+    $(".solution-content").hide();
     
     //choose a question
     //pick a question in the new questions list.  note: list shoudl not be empty
@@ -181,7 +181,7 @@ function askQuestion() {
     timeLeft = timeToGuess;
     questionCountDownInterval = setInterval(countDown, 1000);
     $("#time-left").html("Time Remaining: " + timeLeft);
-};
+}
 
 function countDown(){
     //count down one second
@@ -193,13 +193,13 @@ function countDown(){
         clearInterval(questionCountDownInterval);
         timesUp();
     };
-};
+}
 
 function checkAnswer(){
     console.log("checking the answer!");
     //show or hide content
-    $(".question-area").hide();
-    $(".answer-area").show();
+    $(".question-content").hide();
+    $(".solution-content").show();
     //console log - this 
     console.log("checking the answer of: " + $(this));
     //turn off counter
@@ -210,46 +210,46 @@ function checkAnswer(){
         //add to tally
         correctQs += 1;
         //display that player won
-        $("#answer-result").html("You are correct!")
+        $("#solution-result").html("You are correct!")
         //clear the answer text
-        $("#answer-text").html("");
+        $("#solution-text").html("");
     } else if ($(this).data("isCorrect") === false){
         console.log("wrong answer...");
         //add to tally
         incorrectQs += 1;
         //display that player lost
-        $("#answer-result").html("You are wrong...")
+        $("#solution-result").html("You are wrong...")
         //display correct answer
-        $("#answer-text").html("The correct answer is: " + currentQuestion.correctAnswer);
+        $("#solution-text").html("The correct answer is: " + currentQuestion.correctAnswer);
     } else {
         alert("error: neither correct or incorrect"); //remove for productoin
     };
     //display image of winning answer
-    $("#answer-image").attr("src", currentQuestion.imgSrc);
-    $("#answer-image").attr("alt", currentQuestion.imgAlt);
+    $("#solution-image").attr("src", currentQuestion.imgSrc);
+    $("#solution-image").attr("alt", currentQuestion.imgAlt);
     //set timer for next question
     setTimeout(isGameOver, timeBetweenQuestions);
-};
+}
 
 function timesUp(){
     console.log("time ran out.")
     //add to tally
     unansweredQs += 1;
     //show hide contents
-    $(".question-area").hide();
-    $(".answer-area").show();
+    $(".question-content").hide();
+    $(".solution-content").show();
     //turn off counter
     clearInterval(questionCountDownInterval);
     //display that player failed to answer
-    $("#answer-result").html("You did not answer in time");
+    $("#solution-result").html("You did not answer in time");
     //display correct answer
-    $("#answer-text").html("The correct answer was: " + currentQuestion.correctAnswer);
+    $("#solution-text").html("The correct answer was: " + currentQuestion.correctAnswer);
     //display image of winning answer
-    $("#answer-image").attr("src", currentQuestion.imgSrc);
-    $("#answer-image").attr("alt", currentQuestion.imgAlt);
+    $("#solution-image").attr("src", currentQuestion.imgSrc);
+    $("#solution-image").attr("alt", currentQuestion.imgAlt);
     //set timer for next question
     setTimeout(isGameOver, timeBetweenQuestions);
-};
+}
 
 function isGameOver(){
     //turn off counter
@@ -260,19 +260,19 @@ function isGameOver(){
         //next question
         askQuestion();
     };
-};
+}
 
 function gameOver(){
     //show hide contents
-    $(".answer-area").hide();
-    $(".results-area").show();
+    $(".solution-content").hide();
+    $(".results-content").show();
     //display the results
     $("#results-correct").html("You got " + correctQs + " correct.");
     //display correct answer
     $("#results-incorrect").html("You got " + incorrectQs + " wrong.");
     //display correct answer
     $("#results-unanswered").html("You did not answer " + unansweredQs + " questions.");
-};
+}
 
 //set up the game 
 setupGame();
